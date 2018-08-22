9ec252c988319e0c38710ee3b72b2c8b6602d836
parisa pahlevan <paris.pahlevan@gmail.com>
2018-08-22 13:09:25 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,10 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
-function init() {
+let tracker =0
+function onKeyDownHandler(event) {
+  const key = parseInt(event.detail || event.which)
+  if (tracker ===code.length){
+    window.alert("You found it")
+  }
   // Write your JavaScript code inside the init() function
 
 }
